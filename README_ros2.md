c448c7ad8e607baaee43b814ebbacae51e755773
kaito <kaikichi.j.b@gmail.com>
2025-07-17 10:39:12 -0700

    Add README for setup with ros2. Integrate ros2 in cart_pole_eval.py to analyze cartpole state

---
+++ README_ros2.md
@@ -0,0 +1,80 @@
+# Cart Pole Swing-Up Reinforcement Learning
+## ROS2 Support Branch
+**This branch includes ROS2 (Humble) integration**
+
+## Overview
+This repository enable to implement a **CartPole environment** for training reinforcement learning (RL) agents to solve the classic control problem of balancing a pole on a cart while keeping the cart near the origin. The environment trains agents to **swing the pole up from a downward position to an upright position** and maintain the cart at the center
+
+## Testing Environment
+- AMD Ryzen 7 5700X
+- RTX 3060 Ti
+- CUDA 11.8
+- RAM 32GB
+- Ubuntu 22.04
+- ROS2 humble
+- Python 3.10
+
+## Demo Video
+![Demo](media/cart_pole_rl.gif)  
+
+## Installation and Usage
+
+```bash
+python3.10 -m venv cart-pole-rl
+source cart-pole-rl/bin/activate
+```
+Install pytorch with matching CUDA version (CUDA 11.8 is utilized in this repo)
+```bash
+pip install torch==2.5.1+cu118 torchvision==0.20.1+cu118 torchaudio==2.5.1 -f https://download.pytorch.org/whl/torch_stable.html
+```
+Install Genesis
+```bash
+pip install git+https://github.com/Genesis-Embodied-AI/Genesis.git
+```
+
+Clone Genesis reopsitory and install locally
+```bash
+cd
+git clone https://github.com/Genesis-Embodied-AI/Genesis.git
+cd Genesis
+pip install -e ".[dev]"
+```
+Install rsl_rl library locally
+```bash
+cd
+git clone https://github.com/leggedrobotics/rsl_rl
+cd rsl_rl && git checkout v1.0.2 && pip install -e .
+```
+Clone this repository
+```bash
+cd
+git clone https://github.com/knamatame0729/Cart-Pole-RL-Control.git cart_pole_rl_control
+pip install tensorboard
+```
+
+Run train script
+```bash
+cd cart_pole_rl_control
+python3 cart_pole_train.py
+```
+Run this on the other terminal and follow discription to see train detail
+```bash
+cd cart_pole_rl_control
+tensorboard --logdir logs
+```
+After complete training, run this to watch the training result
+```bash
+python3 cart_pole_eval.py
+```
+
+## References
+- Nikita Rudin, David Hoeller, Philipp Reist, and Marco Hutter.  
+**"Learning to Walk in Minutes Using Massively Parallel Deep Reinforcement Learning."**  
+Proceedings of the 5th Conference on Robot Learning (CoRL 2022), PMLR 164:91–100, 2022.  
+[Link to paper](https://proceedings.mlr.press/v164/rudin22a.html)  
+
+- Genesis Authors. **Genesis: A Generative and Universal Physics Engine for Robotics and Beyond**, December 2024.  
+[https://github.com/Genesis-Embodied-AI/Genesis](https://github.com/Genesis-Embodied-AI/Genesis)
+
+## License
+MIT Liense
--- cart_pole_eval.py
+++ cart_pole_eval.py
@@ -3,6 +3,10 @@ import os
 import pickle
 from importlib import metadata
 
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+
 import torch
 
 try:
@@ -20,18 +24,47 @@ import genesis as gs
 
 from cart_pole_env import CartPoleEnv
 
+class CartPoleStatePublisher(Node):
+    def __init__(self):
+        super().__init__('cart_pole_state_publisher')
+        """ Create publishers """
+        self.cart_pos_pub = self.create_publisher(Float32, 'cart_pos', 10)
+        self.cart_vel_pub = self.create_publisher(Float32, 'cart_vel', 10)
+        self.pole_angle_pub = self.create_publisher(Float32, 'pole_angle', 10)
+        self.pole_vel_pub = self.create_publisher(Float32, 'pole_vel', 10)
+
+    def publish_state(self, cart_pos, cart_vel, pole_angle, pole_vel):
+        """ Publish each state """
+        cart_pos_msg = Float32()
+        cart_pos_msg.data = float(cart_pos.item())
+        self.cart_vel_pub.publish(cart_pos_msg)
+
+        cart_vel_msg = Float32()
+        cart_vel_msg.data = float(cart_vel.item())
+        self.cart_vel_pub.publish(cart_vel_msg)
+
+        pole_angle_msg = Float32()
+        pole_angle_msg.data = float(pole_angle.item())
+        self.pole_angle_pub.publish(pole_angle_msg)
+
+        pole_vel_msg = Float32()
+        pole_vel_msg.data = float(pole_vel.item())
+        self.pole_vel_pub.publish(pole_vel_msg)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--exp_name", type=str, default="cartpole-training")
     parser.add_argument("--ckpt", type=int, default=100)
     args = parser.parse_args()
+    
+    # Initialize ros2
+    rclpy.init()
+    state_publisher = CartPoleStatePublisher()
 
     gs.init()
 
     log_dir = f"logs/{args.exp_name}"
     env_cfg, obs_cfg, reward_cfg, train_cfg = pickle.load(open(f"logs/{args.exp_name}/cfgs.pkl", "rb"))
-    #reward_cfg["reward_scales"] = {}
 
     env = CartPoleEnv(
         num_envs=1,
@@ -52,10 +85,19 @@ def main():
         while True:
             actions = policy(obs)
             obs, rews, dones, infos = env.step(actions)
+
+            # Publish
+            state_publisher.publish_state(env.cart_pos[0], env.cart_vel[0], env.pole_angle[0], env.pole_vel[0])
+
             if dones.any():
                 obs, _ = env.reset()
                 print(f"Episode info: {infos['episode']}")
 
+            rclpy.spin_once(state_publisher, timeout_sec=0.0)
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    finally:
+        rclpy.shutdown()
